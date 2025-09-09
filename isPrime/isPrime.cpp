71493e2380898f28eed8b9bbcbb292e4b870d118
William Miller <Wmilleiv@gmail.com>
2025-09-09 11:39:11 -0400

    cpp working

--- isPrime/isPrime.cpp
+++ isPrime/isPrime.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include <cmath>
 
 bool isPrime(int n){
     if(n<3)
         return true;
-    for(int i{2}; i<((n^(1/2))+1);i+=2){
+    if(n%2==0)
+        return false;
+    for(int i{3}; i<std::pow(n,0.5)+1;i+=2){
         if(n%i==0)
             return false;
     }
