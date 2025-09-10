5034d394c2ced130ffbe970f6f71c3c4de04c39b
William Miller <Wmilleiv@gmail.com>
2025-09-10 14:17:14 -0400

    java done

--- isPrime/isPrime.java
+++ isPrime/isPrime.java
@@ -1,4 +1,25 @@
-public static class isPrime{
-    public static int main(Sting[] args){
+import java.util.Scanner;
+import java.lang.Math;
+
+public class isPrime{
+    public static boolean isPrime(int n){
+        if(n<3)return true;
+        if(n%2==0)return false;
+        for(int i=3;i<Math.pow(n,0.5)+1;i+=2){
+            if(n%i==0)return false;
+        }
+        return true;
+    }
+    public static void main(String[] args){
+    
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int in = sc.nextInt();
+
+        if(isPrime(in)){
+            System.out.println(in + " is prime.");
+        }else{
+            System.out.println(in + " is not prime.");
+        }
     }
 }
