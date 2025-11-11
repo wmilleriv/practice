43afb0ea98857074e04383b1659fadb8fe8e54d7
William Miller <wmilleiv@gmail.com>
2025-11-11 10:12:21 -0500

    looping factorial python

--- factExamples/loopFact.py
+++ factExamples/loopFact.py
@@ -1,6 +1,11 @@
 def fact(n):
     product=1
-    for i in range(n):
+    for i in range(1,n+1):
         product*=i
 
+    return product
+
+print("Enter an integer: ")
+k=input()
+print(str(fact(int(k))))
 
