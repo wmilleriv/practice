549bd849746723c45709e981c9c51f1422824344
William Miller <wmilleiv@gmail.com>
2025-11-18 10:16:50 -0500

    Cpp Number guessing sample

--- numGuess/numGuess.cpp
+++ numGuess/numGuess.cpp
@@ -8,7 +8,21 @@ int main(){
 	std::uniform_int_distribution<int> dist{0,1000};
 	int num{dist(mt)};
 
-	std::cout << num;
+
+	int guess{};
+	std::cout << "Enter an Integer between 0 and 1000: \n";
+	std::cin >> guess;
+
+	while(guess!=num){
+		if(guess>num){
+			std::cout << "Too high. Guess again: \n";
+		}
+		if(guess<num){
+			std::cout << "Too Low. Guess again: \n";
+		}
+		std::cin >> guess;
+	}
+	std::cout << "Congratulations! You win!\n";
 
 	return 0;
 }
