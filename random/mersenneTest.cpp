a4553fbb716384a6870b5375407295cf4920dbe9
William Miller <wmilleiv@gmail.com>
2025-11-19 11:07:04 -0500

    seeded dice

--- random/mersenneTest.cpp
+++ random/mersenneTest.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 #include <random>
+#include <chrono>
+
 
 int main(){
 
-	std::mt19937 mt{};
+	std::mt19937 mt{static_cast<std::mt19937::result_type>(
+			std::chrono::steady_clock::now().time_since_epoch().count()
+			)};
+	std::uniform_int_distribution die6{1,6};
 
 	for(int i{0};i<40;i++){
-		std::cout << mt()<<'\t';
+		std::cout << die6(mt)<<'\t';
 		if(i%5==0)
 			std::cout << '\n';
 	}
