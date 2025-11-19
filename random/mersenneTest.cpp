03076e375cc986d8e44e5e9e3fb908c6b47e7221
William Miller <wmilleiv@gmail.com>
2025-11-19 11:15:40 -0500

    remove chrono, add random device

--- random/mersenneTest.cpp
+++ random/mersenneTest.cpp
@@ -5,9 +5,10 @@
 
 int main(){
 
-	std::mt19937 mt{static_cast<std::mt19937::result_type>(
-			std::chrono::steady_clock::now().time_since_epoch().count()
-			)};
+	//std::mt19937 mt{static_cast<std::mt19937::result_type>(
+	//		std::chrono::steady_clock::now().time_since_epoch().count()
+	//		)};
+	std::mt19937 mt{std::random_device{}()};
 	std::uniform_int_distribution die6{1,6};
 
 	for(int i{0};i<40;i++){
