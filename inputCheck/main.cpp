acaa76407d8274f8c58c4689993987c046e26e55
William Miller <wmilleiv@gmail.com>
2025-11-21 13:10:33 -0500

    input validation sample

--- inputCheck/main.cpp
+++ inputCheck/main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 #include <string>
 
+int getInteger(){
+	int k{};
+	std::cout << "Enter an Integer: ";
+	std::cin >> k;
+	return k;
+}
+
+std::string getString(){
+	std::string str{};
+	std::cout << "Enter a string: ";
+	std::cin >> str;
+	return str;
+}
+
 int main(){
 
-	testInput{}
+	int testInput{getInteger()};
 	return 0;
 }
