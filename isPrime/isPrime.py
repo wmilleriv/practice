89c545f5715a5b43e8be64ab1ee27e6b18c031bc
William Miller <wmilleiv@gmail.com>
2026-01-09 08:05:12 -0500

    refactor for speed

--- isPrime/isPrime.py
+++ isPrime/isPrime.py
@@ -2,22 +2,33 @@ import csv
 
 def isPrime(n):
     for num in nums:
-        if n%int(num)==0:
+        if n%int(num.strip())==0:
             return False
+    print(str(n) + "is prime")
     return True
 
+
+def saveList(primes):
+    with open('primes.csv','a',newline='') as p:
+        writer=csv.writer(p)
+        writer.writerow(primes)
+
 nums=[]
-with open('primes.txt','r') as p:
+with open('primes.csv','r',newline='') as p:
     reader=csv.reader(p) 
     for row in reader:
         nums.extend(row)
  
-primes=[]
-for i in range((int(nums[-1])+2),(int(nums[-1])**2)):
-    if(isPrime(i)):
-       primes.append(i)
 
-with open('primes.txt','a') as p:
-    for prime in primes:
-       p.write(",")
-       p.write(str(prime))
+for i in range((int(nums[-1])+2),(int(nums[-1])**2)):
+    primes=[]
+    for j in range(1000):
+        if(isPrime(i)):
+            primes.append(i)
+    saveList(primes)
+
+
+def saveList(primes):
+    with open('primes.csv','a',newline='') as p:
+        writer=csv.writer(p)
+        writer.writerow(primes)
