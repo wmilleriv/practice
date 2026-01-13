cd01979c1216a0d20f667e1adb63f658e9970452
William Miller <wmilleiv@gmail.com>
2026-01-13 14:54:16 -0500

    encoding for ascii and little kids.

---
+++ fileIO/kidGen.py
@@ -0,0 +1,24 @@
+import random
+import string
+
+
+MSG="This is written using ASCII. ASCII is one of the oldest ways to translate computer bytes to English characters. This translation is known as encoding. ASCII is still widely used as a subset of modern encoding algorithms like UTF-8 but it has a significant drawback of not working with many foreign language alphabets."
+encodedMsg=""
+
+
+
+def write(msg):
+    with open('ascii.txt', 'w') as f:
+        f.write(msg)
+
+def encode(MSG, encodedMsg):
+    for i in range(len(MSG)):
+            encodedMsg+=str(ord(MSG[i])) + "  "
+    write(encodedMsg)
+
+
+
+encode(MSG, encodedMsg)
+
+   
+
