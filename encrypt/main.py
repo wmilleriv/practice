28e5f039a95e84cb162795e5b277b64451cfbcc1
William Miller <wmilleiv@gmail.com>
2026-01-23 12:40:42 -0500

    decode function done

--- encrypt/main.py
+++ encrypt/main.py
@@ -1,4 +1,5 @@
 MESSAGE_SAMPLE="This is a sample message for testing. It should be replaced later"
+ENCRYPTED_SAMPLE="65 66 67 68 69 70 71 72"
 def encode(msg):
     encodedMsg=""
     for char in list(msg):
@@ -8,7 +9,9 @@ def encode(msg):
 
 def decode(encoded_msg):
     msg=""
+    for char in encoded_msg.split():
+        msg+=str(chr(int(char)))
     return msg
 
 
-print(encode(MESSAGE_SAMPLE))
+print(decode(ENCRYPTED_SAMPLE))
