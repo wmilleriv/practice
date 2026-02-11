ff510564d3cc6044c38bc7ed49250c4dcfbafebe
William Miller <wmilleiv@gmail.com>
2026-02-11 07:40:43 -0500

    longer runtime

--- isPrime/isPrime.py
+++ isPrime/isPrime.py
@@ -7,20 +7,20 @@ def isPrime(n,knownPrimes):
 
 
 def saveList(primes):
-    with open('primes.txt','a') as f:
+    with open('primes.txt','a',encoding="utf-8", newline=None) as f:
         for item in primes:
-            f.write(str(item) + " ")
+            f.write((str(item) + " ").strip('\n'))
 
 def read():
     knownPrimes=[]
-    with open('primes.txt','r') as f:
+    with open('primes.txt','r', encoding='utf-8', newline=None) as f:
         for prime in f.read().split():
             knownPrimes.append(prime)
     return knownPrimes;
     
 knownPrimes=read()
 newPrimes=[]
-for i in range((int(knownPrimes[-1])+2),int(knownPrimes[-1])+1000):
+for i in range((int(knownPrimes[-1])+2),int(knownPrimes[-1])+1000000):
     if(isPrime(i, knownPrimes)):
         newPrimes.append(i)
 saveList(newPrimes)
