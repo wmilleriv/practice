cc86937bd031d368ff1b84c476f1aa69256036cd
William Miller <wmilleiv@gmail.com>
2026-03-09 18:29:53 -0400

    pi Clicker

--- web/piClicker/pi.js
+++ web/piClicker/pi.js
@@ -1,15 +1,16 @@
 const update=document.getElementById('update');
 var piSpan=document.getElementById('pi');
+var termSpan=document.getElementById('term');
 
-var term=parseFloat(piSpan.innerHTML)
+var term=parseFloat(termSpan.innerHTML)
 var pi=parseFloat(piSpan.innerHTML);
 var clicks=0;
 function addTerm(){
 	clicks=clicks+1;
-	term=parseFloat(4*((-1)**clicks)/(2*clicks+1));
-	pi=pi+term;
-	console.log(pi);
+	term=parseFloat(4*((-1)**clicks)/(2*clicks+1));//clicks is incremented to get next term
+	pi=pi+term
 	piSpan.innerHTML=pi
+	termSpan.innerHTML=term;
 }
 
 update.addEventListener('click', addTerm);
