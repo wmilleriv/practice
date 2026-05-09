3e07d9a9f7476f3bfca4486849c567a17548084a
William Miller <Wmilleriv@outlook.com>
2026-05-09 11:26:17 -0400

    most functions

--- calc/calc.cpp
+++ calc/calc.cpp
@@ -1,5 +1,42 @@
 #include <iostream>
+int comp(x,y){
+	return x==y;
+} 
+
+int add(int x, int y){
+	return x+y;
+}
+
+int sub(int x, int y){
+	return x+-y;
+}
+
+int mult(int x, int y, int z=0){
+	if(comp(z,0)
+		z=x;
+	if(comp(y,1))
+		return x;
+	else
+		return(mult(x+z,y-1),z);
+}
+
+int div(int x, int y, int z=0){
+	if(comp(x,0)
+			return ;
+}
+
+
+
+int menu(){
+	std::cout << "";
+	return 0;
+
+}
+
 
 int main(){
+	std::cout << add(100,5) << '\n';
+	std::cout << sub(100,5) << '\n';
+	std::cout << mult(100,5) << '\n';
 	return 0;
 }
