81a9a534fa183ac21f8e694417d99561d43191a3
William Miller <Wmilleriv@outlook.com>
2026-05-10 07:39:35 -0400

    menu

--- calc/calc.cpp
+++ calc/calc.cpp
@@ -27,7 +27,7 @@ int mult(int x, int y, int z=0){
 	if(comp(y,1))
 		return x;
 	else
-		return(mult(x+z,y-1,z));
+		return(mult(x+z,sub(y,1),z));
 }
 
 int divide(int x, int y, int z=0){	
@@ -36,15 +36,33 @@ int divide(int x, int y, int z=0){
 	if(comp(x,0))
 		return z+1;
 	else
-		return(divide(x-y,y,z+1));
+		return(divide(sub(x,y),y,z+1));
 
 
 }
 
-
+int pow(int x, int y, int z=0){
+	if(comp(y,0))
+		return 1;
+	if(comp(z,0))
+		z=x;
+	if(comp(y,1))
+		return x;
+	else
+		return pow(mult(x,z),sub(y,1), z);
+}
 
 int menu(){
-	std::cout << "";
+	int operation{0};
+	std::cout << "Choose an operation\n";
+	std::cout <<"--------------------------\n";
+	std::cout <<"1) Addition\n";
+	std::cout <<"2) Subtraction\n";
+	std::cout <<"3) Multiplication\n";
+	std::cout <<"4) Division\n";
+	std::cout <<"5) Exponentiation\n";
+	std::cout <<"--------------------------\n";
+	std::cin >> 
 	return 0;
 
 }
@@ -55,5 +73,6 @@ int main(){
 	std::cout << sub(100,5) << '\n';
 	std::cout << mult(100,5) << '\n';
 	std::cout << divide(100,7) << '\n';
+	std::cout << pow(3,4) <<'\n';
 	return 0;
 }
