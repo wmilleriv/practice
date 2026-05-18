d63e4b68f61be9601b987fb163523ef3140b66be
William Miller <wmilleiv@gmail.com>
2026-05-18 10:50:04 -0400

    main functions

--- calc/noLoopPyCalc.py
+++ calc/noLoopPyCalc.py
@@ -8,12 +8,11 @@ def subtract(x,y):
 	return add(x,-y)
 		
 def lessThan(x,y):
-	if(comp(y,0):
-		return false
-	if(comp(x,0):
-		return true
+	if(isEqual(y,0)):
+		return False
+	if(isEqual(x,0)):
+		return True
 	return(lessThan(subtract(x,1),subtract(y,1)))
-
 
 def multiply(x,y,z=0):#doesn't work for 0
 	if(isEqual(z,0)):
@@ -23,9 +22,23 @@ def multiply(x,y,z=0):#doesn't work for 0
 	return multiply(add(x,z),subtract(y,1),z)
 	
 def divide(x,y,z=0):
+		if(lessThan(x,y)):
+			return z
+		if isEqual(x,0):
+			return add(z,1)
+		return divide(subtract(x,y),y,add(z,1))
 	
+def exponentiate(x,y, z=0):
+	if(isEqual(y,0)):
+		return 1
+	if(isEqual(z,0)):
+		z=x
+	if(isEqual(y,1)):
+		return x
+	return exponentiate(multiply(x,z),subtract(y,1), z)
 	
-	
-print(multiply(3,1))
+print(exponentiate(3,1))
+print(exponentiate(3,3))
+print(exponentiate(17,0))
 	
 	
