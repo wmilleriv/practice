0348e0a4fc9128a6050d9aa921bd238fadf15a68
William Miller <wmilleiv@gmail.com>
2026-05-18 09:25:09 -0400

    non looping py progress

--- calc/noLoopPyCalc.py
+++ calc/noLoopPyCalc.py
@@ -1,11 +1,19 @@
 def isEqual(x,y):
 	return x==y
-	
+
 def add(x,y):
 	return x+y
 	
 def subtract(x,y):
 	return add(x,-y)
+		
+def lessThan(x,y):
+	if(comp(y,0):
+		return false
+	if(comp(x,0):
+		return true
+	return(lessThan(subtract(x,1),subtract(y,1)))
+
 
 def multiply(x,y,z=0):#doesn't work for 0
 	if(isEqual(z,0)):
@@ -13,6 +21,9 @@ def multiply(x,y,z=0):#doesn't work for 0
 	if(isEqual(y,1)):
 		return x
 	return multiply(add(x,z),subtract(y,1),z)
+	
+def divide(x,y,z=0):
+	
 	
 	
 print(multiply(3,1))
